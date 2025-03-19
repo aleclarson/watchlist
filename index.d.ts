72265ae3a7bc0b81fc7745a08b67d7dfa11c3782
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-03-19 14:13:17 -0400

    fix(types): remove `run` type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,3 @@
-import type { BaseEncodingOptions } from 'fs';
-import type { ExecOptions } from 'child_process';
-
 type Arrayable<T> = T[] | T;
 type Promisable<T> = Promise<T> | T;
 
@@ -14,9 +11,3 @@ export interface Options {
 export type Handler = () => Promisable<any>;
 
 export function watch(dirs: string[], handler: Handler, opts?: Partial<Options>): Promise<void>;
-
-export function run(command: string, options: { encoding: 'buffer' | null } & ExecOptions): Promise<void>;
-export function run(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<void>;
-export function run(command: string, options: { encoding: BufferEncoding } & ExecOptions): Promise<void>;
-export function run(command: string, options: ExecOptions): Promise<void>;
-export function run(command: string, options: (BaseEncodingOptions & ExecOptions) | undefined | null): Promise<void>;
